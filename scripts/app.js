da21c06db079da0382708d5d3f0745ad5fa29813
Umang Mistry <umangmistry2910@gmail.com>
2024-07-17 19:55:13 -0400

    Modify

--- scripts/app.js
+++ scripts/app.js
@@ -1,66 +1,55 @@
-new Vue({
-    el: '#random-fact',
-    data: {
-        fact: ''
+const app = Vue.createApp({
+    data() {
+        return {
+            weather: {
+                temperature: '',
+                wind: '',
+                description: ''
+            },
+            city: 'London, Ontario',
+            word: '',
+            dictionary: {
+                word: '',
+                phonetic: '',
+                partOfSpeech: '',
+                definition: ''
+            },
+            fact: ''
+        };
+    },
+    created() {
+        this.getWeather();
+        this.getRandomFact();
     },
     methods: {
-        fetchFact() {
+        getWeather() {
+            fetch(`https://goweather.herokuapp.com/weather/${this.city}`)
+                .then(response => response.json())
+                .then(data => {
+                    this.weather.temperature = data.temperature;
+                    this.weather.wind = data.wind;
+                    this.weather.description = data.description;
+                });
+        },
+        defineWord() {
+            fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${this.word}`)
+                .then(response => response.json())
+                .then(data => {
+                    const entry = data[0];
+                    this.dictionary.word = entry.word;
+                    this.dictionary.phonetic = entry.phonetics[0].text;
+                    this.dictionary.partOfSpeech = entry.meanings[0].partOfSpeech;
+                    this.dictionary.definition = entry.meanings[0].definitions[0].definition;
+                });
+        },
+        getRandomFact() {
             fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
                 .then(response => response.json())
                 .then(data => {
                     this.fact = data.text;
                 });
         }
-    },
-    mounted() {
-        this.fetchFact();
     }
 });
 
-new Vue({
-    el: '#weather',
-    data: {
-        city: 'London Ontario',
-        weatherInfo: ''
-    },
-    methods: {
-        fetchWeather() {
-            fetch(`https://goweather.herokuapp.com/weather/London%20Ontario`)
-                .then(response => response.json())
-                .then(data => {
-                    this.weatherInfo = `Temperature: ${data.temperature}, Wind: ${data.wind}, Description: ${data.description}`;
-                });
-        },
-        updateCityWeather() {
-            const city = document.getElementById('city').value;
-            this.city = city;
-            this.fetchWeather();
-        }
-    },
-    mounted() {
-        this.fetchWeather();
-    }
-});
-
-new Vue({
-    el: '#dictionary',
-    data: {
-        word: '',
-        definitionInfo: ''
-    },
-    methods: {
-        fetchDefinition() {
-            const word = document.getElementById('word').value;
-            fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/Bottle`)
-                .then(response => response.json())
-                .then(data => {
-                    if(data.title) {
-                        this.definitionInfo = 'No definition found.';
-                    } else {
-                        const entry = data[0];
-                        this.definitionInfo = `Word: ${entry.word}, Phonetic: ${entry.phonetic}, Part of Speech: ${entry.meanings[0].partOfSpeech}, Definition: ${entry.meanings[0].definitions[0].definition}`;
-                    }
-                });
-        }
-    }
-});
+app.mount('#app');
